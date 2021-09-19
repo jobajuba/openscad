461cf2a5a41a51e153814c1d2aab59e3587ce336
Torsten Paul <Torsten.Paul@gmx.de>
2021-09-19 02:52:49 +0200

    Add JSON import.

--- src/MainWindow.cc
+++ src/MainWindow.cc
@@ -209,6 +209,7 @@ MainWindow::MainWindow(const QStringList &filenames)
 
 	const QString importStatement = "import(\"%1\");\n";
 	const QString surfaceStatement = "surface(\"%1\");\n";
+	const QString importFunction = "data = import(\"%1\");\n";
 	knownFileExtensions["stl"] = importStatement;
 	knownFileExtensions["3mf"] = importStatement;
 	knownFileExtensions["off"] = importStatement;
@@ -217,6 +218,7 @@ MainWindow::MainWindow(const QStringList &filenames)
 	knownFileExtensions["amf"] = importStatement;
 	knownFileExtensions["dat"] = surfaceStatement;
 	knownFileExtensions["png"] = surfaceStatement;
+	knownFileExtensions["json"] = importFunction;
 	knownFileExtensions["scad"] = "";
 	knownFileExtensions["csg"] = "";
 
--- src/import.h
+++ src/import.h
@@ -9,3 +9,4 @@ class Polygon2d *import_svg(const std::string &filename, const double dpi, const
 #ifdef ENABLE_CGAL
 class CGAL_Nef_polyhedron *import_nef3(const std::string &filename, const Location &loc);
 #endif
+class Value import_json(const std::string& filename, class EvaluationSession *session, const Location& loc);
